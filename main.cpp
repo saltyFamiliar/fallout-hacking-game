ef962eb3f13291b45b9b29786bb4a62df6f3267b
salty <watchmaker3896@gmail.com>
2022-05-27 20:18:40 -0500

    DisplayView(prev. PrintFormatted) shows mem column

--- main.cpp
+++ main.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 #include <ncurses.h>
-#include "hacking_game.h"
 #include <string>
 #include <locale.h>
+#include <iomanip>
+#include "hacking_game.cpp"
 
-#define WIDTH 20
+#define WIDTH 32
 #define HEIGHT 10
 #define TRIES 4
 #define WORD_LENGTH 5
 #define DICT_FILE "dict.txt"
 #define WORD_COUNT 6
 #define PUZZLE_OFFSET_Y 2
-#define PUZZLE_OFFSET_X 0
+#define PUZZLE_OFFSET_X 8
+#define MEM_DIGITS 4
 
-void PrintFormatted(ViewContent contents) {
+// Prints string section, moving cursor to next line on each newline encounter.
+// Returns any chars left over in case string does end in newline. Function is 
+// needed in order for puzzle to be printed according to PUZZLE_OFFSET on each 
+// line. (Regular printw starts each line at x=0.)
+std::string PrintGridSection(std::string section) {
+    int temp_y;
+    int temp_x;
+    std::string char_line = "";
+    for (int i = 0; i < section.length(); ++i) {
+        auto next_char = section[i];
+        char_line += next_char;
+
+        if (next_char == '\n') {
+            printw(char_line.c_str());
+            getyx(stdscr, temp_y, temp_x);
+            move(temp_y, PUZZLE_OFFSET_X);
+            char_line = "";
+        }
+    }
+    return char_line;
+}
+
+// Outputs ViewContent in a formatted manner. Displays memory address column.
+void DisplayView(ViewContent contents) {
     int y;
     int x;
     getyx(stdscr, y, x);
@@ -21,36 +46,53 @@ void PrintFormatted(ViewContent contents) {
     
     clear();
 
-    std::string attempts_msg = "Attempt(s) Left: ";
+    // Attempts left status
+    std::string attempts_msg = "Attempt(s) Left:";
     for (int i = 0; i < contents.attempts_left; ++i) {
-        attempts_msg += "■";
+        attempts_msg += " ■";
+    }
+    printw(attempts_msg.c_str());
+    
+    // Print memory address column on left.
+    size_t addr = (size_t)&contents.char_grid_sections;
+    for (int i = 0; i < HEIGHT; ++i) {
+        move(i + PUZZLE_OFFSET_Y, 0);
+
+        std::stringstream stream;
+        stream << std::hex << addr + (i * (WIDTH + 1));
+        std::string addr_str(stream.str());
+        
+        addr_str = addr_str.substr(addr_str.length() - MEM_DIGITS);
+        std::transform(addr_str.begin(), addr_str.end(), addr_str.begin(), toupper);
+
+        printw(("0x" + addr_str).c_str());
     }
 
-    printw(attempts_msg.c_str());
     move(PUZZLE_OFFSET_Y, PUZZLE_OFFSET_X);
 
-    // char_grid is given as either one whole section or 3. If 3, middle needs
-    // to be highlighted to indicate cursor is at start of a word
-    if (contents.char_grid_sections.size() == 1) {
-        printw(contents.char_grid_sections[0].c_str());
-    } else {
-        printw(contents.char_grid_sections[0].c_str());
+    // Print first char_grid_section, saving anything left over to left_over.
+    // If more than one grid_section in ViewContent, second section is assumed
+    // to be word and is formatted accordingly.
+    std::string left_over = PrintGridSection(contents.char_grid_sections[0]);
+    if (contents.char_grid_sections.size() > 1) {
+        printw(left_over.c_str());
+
         attron(COLOR_PAIR(1));
         printw(contents.char_grid_sections[1].c_str());
         attroff(COLOR_PAIR(1));
-        printw(contents.char_grid_sections[2].c_str());
+
+        PrintGridSection(contents.char_grid_sections[2]);
     }
-    printw("\n");
     
     // Print status column starting from bottom of the puzzle
     for (int i = 0; i < contents.status_col.size(); ++i) {
         move((PUZZLE_OFFSET_Y + HEIGHT) - i - 1, PUZZLE_OFFSET_X + WIDTH + 2);
         printw(contents.status_col[contents.status_col.size() - i - 1].c_str());
     }
+
     move(y, x);
     refresh();
 }
-
 
 int main() { 
     // Necessary for unicode
@@ -77,14 +119,14 @@ int main() {
 
     // offset needed because puzzle does not start at 0, 0
     auto offset = Position(PUZZLE_OFFSET_Y, PUZZLE_OFFSET_X);
-    auto cursor = Position(PUZZLE_OFFSET_Y, PUZZLE_OFFSET_Y);
+    auto cursor = Position(PUZZLE_OFFSET_Y, PUZZLE_OFFSET_X);
 
     // Move cursor to start
     move(cursor.y_, cursor.x_);
 
     // Display puzzle initial contents
     ViewContent contents = puzzle.View(cursor, offset);
-    PrintFormatted(contents);
+    DisplayView(contents);
     move(cursor.y_, cursor.x_);
 
     // Update changes to window
@@ -115,7 +157,7 @@ int main() {
         case '\n':
             if (puzzle.Update(cursor, offset)) {
                 contents = puzzle.View(cursor, offset);
-                PrintFormatted(contents);
+                DisplayView(contents);
                 getch();
             } else {
                 break;
@@ -126,9 +168,8 @@ int main() {
         }
         move(cursor.y_, cursor.x_);
         contents = puzzle.View(cursor, offset);
-        PrintFormatted(contents);
+        DisplayView(contents);
     }
-
 
     return 0;
 }
